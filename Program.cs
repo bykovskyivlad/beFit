359d649fcc1603a84ff0b82180d29304e2c71ece
Vladyslav <bikovskijvladislav@gmail.com>
2025-11-05 15:10:24 +0100

    Initial commit - BeFit project

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using beFit.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace beFit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Connection string -> SQLite
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                 ?? "Data Source=befit.db";
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(connectionString));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+                options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication(); // 🚀 koniecznie dodaj
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
